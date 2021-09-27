e6099ab8f174e33bc75c3263c323022ec3a04485
mirkonikic <4370557@gmail.com>
2021-09-27 17:35:21 +0200

    initialization complete

--- src/main.s
+++ src/main.s
@@ -38,6 +38,60 @@
 
 ;3 different interrupts: NMI ($FFFA), Reset Vector ($FFFC), BRK/IRQ ($FFFE)
 
+;Byte     Contents
+;---------------------------------------------------------------------------
+;0-3      String "NES^Z" used to recognize .NES files.
+;4        Number of 16kB ROM banks.
+;5        Number of 8kB VROM banks.
+;6        bit 0     1 for vertical mirroring, 0 for horizontal mirroring.
+;         bit 1     1 for battery-backed RAM at $6000-$7FFF.
+;         bit 2     1 for a 512-byte trainer at $7000-$71FF.
+;         bit 3     1 for a four-screen VRAM layout. 
+;         bit 4-7   Four lower bits of ROM Mapper Type.
+;7        bit 0     1 for VS-System cartridges.
+;         bit 1-3   Reserved, must be zeroes!
+;         bit 4-7   Four higher bits of ROM Mapper Type.
+;8        Number of 8kB RAM banks. For compatibility with the previous
+;         versions of the .NES format, assume 1x8kB RAM page when this
+;         byte is zero.
+;9        bit 0     1 for PAL cartridges, otherwise assume NTSC.
+;         bit 1-7   Reserved, must be zeroes!
+;10-15    Reserved, must be zeroes!
+;16-...   ROM banks, in ascending order. If a trainer is present, its
+;         512 bytes precede the ROM bank contents.
+;...-EOF  VROM banks, in ascending order.
+;---------------------------------------------------------------------------
+
+;P (status register)	N V   B D I Z C		#5 IS UNUSED
+;			0 0 0 0 0 0 0 0
+;			7 6 5 4 3 2 1 0
+
+;ROM - game code or graphics
+;RAM - program memory, code for the game as in running process
+;CHR - character memory, data for graphics
+;CPU - central processing unit
+;PPU - picture processing unit, graphics chip
+;APU - audio processing unit
+
+;RAM map
+;$0000 - $00FF	Zero Page
+;$0100 - $019F	Data to be copied to nametable during next VBLANK
+;$01A0 - $01FF	Stack
+;$0200 - $02FF	Data to be copied to OAM during next VBLANK
+;$0300 - $03FF	Variables used by sound player
+;$0400 - $07FF	Arrays and less often accessed global variables
+
+;OAM - Memory inside PPU that contains a display list of up to 64 sprites, where each sprite occupies 4bytes, byte0 - Yposition of sprite, byte1 - index number of sprite in selected pattern table in PPUCTRL register, byte2 - attributes, byte3 - Xposition of sprite			Sprites are numbered 0-63: sp0:00-03, sp1:04-07...
+
+;nametable - 1024 byte area of memory used by PPU to lay out backgrounds. Each byte in the nametable controls one 8x8 pixel, character cell, and each nametable has 30rows of 32tiles each, for 960 bytes. The rest is used by each nametables attribute table. 
+;|$2000|$2400|
+;-------------
+;|$2800|$2C00|
+
+;attribute table - 64 byte array at the end of each nametable that controls which palette is assigned to each part of background, each starting at: $23C0, $27C0, $2BC0, $2FC0 respectively, colors in order of: xx000000 - top left, 00xx - top right, bottom left and bottom right
+
+;character - CHR - 
+
 ;iNES header
 .segment "HEADER"
 	;0->3 Must not be modified
@@ -45,17 +99,193 @@
 	.byte $1A	;Necessary character break
 
 	;4->8->15 Can be modified
-	.byte $01	;Number of PRG-ROM blocks
-	.byte $01	;Number of CHR-ROM blocks
-	.byte $00	;Lower half of mapper number & flags
-	.byte $00	;Upper half of mapper number & flags
+	.byte $02	;Number of PRG-ROM blocks(size in units of 16kB)
+	.byte $01	;Number of CHR-ROM blocks(size in units of 8kB)
+	
+	;flag6: lsb 		- mirroring(0=h, 1=v)
+	;	000000x0	- cartridge contains battery backed RAM (1)
+	;	00000x00	- 512 byte trainer at $7000-$71FF (1)
+	;	0000x000	- Ignore mirroring control, provide four VRAM(1)
+	;	xxxx0000	- lower byte nibble of the mapper number
+	.byte %00000001	;Lower half of mapper number & flags
+	;flag7
+	;	0000000x	- VS Unisystem (1)
+	;	000000x0	- PlayChoice-10 (1)
+	;	0000xx00	- if eq 2, 8-15 are in NES 2.0 format
+	;	xxxx0000	- Upper nibble of mapper number
+	.byte %00000000	;Upper half of mapper number & flags
+	
 	;Not used, must be filled with all zeros
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, "M", "I", "R", "K", "O"
 
 .segment "ZEROPAGE"
-
 .segment "STARTUP"
+RST:
+	SEI	;Disables all interrupts
+	CLD	;disable decimal mode, NES doesnt, but 6502 does
+
+	;Disable sound IRQ (Interrupt request)
+	LDX #$40	;if bit 6 is set 1, audio is disabled 01000000
+	STX $4017	;APU Frame Counter
+
+	;Initialize stack register
+	LDX #$FF	;Because stack starts at $0100 and ends at $01FF so when we set S (stack pointer) to point to FF, it decreases to FE, than FD...
+	TXS		;Move data from X to S register, now PHA and PLA
+
+	INX		;Zero out X
+	;Set PPU Registers
+	STX $2000	;PPUCTRL
+	STX $2001	;PPUMASK
+	STX $4010	;Disable PCM Channel, so we dont get weird sounds
+
+	;wait for PPU init, try to draw something
+	;read from $2002, PPUSTATUS, bit 7 shows if we are in VBLANK
+:	
+	LDA $2002
+	AND #%10000000
+	BPL :-		;branch if positive, :- (branch to previous label)
+	
+	AND #$00	;Clear out the A register
+	
+	;Clear out the memory
+clearmemory:
+	STA $0000, X	;0000->00FF
+	STA $0100, X	;0100->01FF
+	STA $0200, X	;...
+	STA $0300, X	;
+	STA $0400, X	;
+	STA $0500, X	;
+	STA $0600, X	;
+	STA $0700, X	;0700->07FF	;0800 je kraj RAM memorije
+	
+	;PPU Update, deo memorije koji sadrzi Sprites, zato necemo 0
+	LDA #$FF
+	STA $0200, X	;0200->02FF
+	LDA #$00	
+
+	INX
+	BNE clearmemory ;Kada se desi roll over sa FF na vrednost 00, registera X, tada se podesi Zero Flag, jer je rezultat operacije bio 0
+
+	;PPU memorija mora da se refreshuje svaki frejm jer postoji memory decay, zbog kog se gube vrednosti u memoriji ako se ne refreshuje duze vreme
+
+	;Read VBLANK Again, to make sure its realy ready
+:	
+	LDA $2002
+	AND #%10000000
+	BPL :-		;branch if positive, :- (branch to previous label)
+
+	;Updating Sprite memory
+	LDA #$02	;most significant memory
+	STA $4014 	;Ubaci high byte of display address, onda ce $0200-$02ff biti prebacen u OAM
+	NOP		;da bi sacekali da PPU prebaci dspmem u OAM
+	
+;PPU memory map
+;$0000-$0FFF	pattern table 0
+;$1000-$1FFF	pattern table 1
+;$2000-$23FF	nametable 0
+;$2400-$27FF	nametable 1
+;$2800-$2BFF	nametable 2
+;$2C00-$2FFF	nametable 3
+;$3000-$3EFF	mirror 2000-2eff
+;$3F00-$3F1F	palette RAM indexes
+;$3F20-$3FFF	mirrors 3f00-3f1f
+
+	;CPU ne moze da upisuje u VRAM od PPU-a, pa je potrebno preko registara da kaze gde u PPU-u zeli da pise, prvo pisati u PPUADDR addressu, gornji i donji byte addresse koja je 16bit addressa zbog 16 bit magistrale, pa ponovno pise u PPUDDATA da popuni VRAM, valid addresses are 0000-3FFF 
+	;Palette information
+	LDA #$3F	;higher byte of address
+	STA $2006
+	LDA #$00	;lower byte of address
+	STA $2006
+	
+	LDX #$00	;loop counter
+palette_load:
+	;two sets of pallets bckg and front, (4x4pallets)x2sets - 32 colors, 0 is bckg always
+	LDA palette_data_bckg, X	;preci ce u sprite data	
+	STA $2007
+	INX
+	CPX #$20	;32 decimal
+	BNE palette_load
+
+	;Kako sam podesio u $4014 da display memory bude u #$20 high byte-u, znaci da ce 2000-20ff biti sprite data memory
+	LDX #$00
+sprite_load:
+	;byte0 - Y coordinate
+	;byte1 - index number of sprite in main.chr
+	;byte2 - attributes...
+	;byte3 - X coordinate
+	LDA sprite_data, X
+	STA $0200, X		;adresa koju sam odredio da je sprite data
+	INX
+	CPX #$20	;svaki sprite se sastoji od 4 byte-a, ispisao sam to gore
+	BNE sprite_load
+
+;Enable interrupts, da bi PPU krenuo da crta
+	CLI
+	
+	LDA #%10010000	
+	;First bit: Generate NMI on start of every VBLANK
+	;Second bit: Sprite size - (0)8x8, (1)8x16, bckg uses second charset
+	STA $2000	;PPUCTRL register
+
+	;turn on drawing
+	LDA #%00011110
+	;000 - Emphasize colors
+	;First set bit: show sprites
+	;Second set bit: show background
+	;Third set bit: show sprites in leftmost 8 pixels of screen
+	;Fourth set bit: show background in leftmost 8 pixels of screen
+	;0 - grayscale (0) normal, (1) gray
+	STA $2001	;PPUMASK
+
+;main loop
+Loop:
+	JMP Loop
+
+NMI:
+	;copy data in 0200 to PPU
+	LDA #$02	;copy sprite data from $0200 into PPU OAM
+	STA $4014
+	RTI	;ret interrupt (usually its rts, but for interrupts its rti)
+
+; BCKG COLOR PALLET: A, B, C, D gde svako slovo ima po 4 boje
+; FRNT COLOR PALLET: D, E, F, G gde svako slovo ima po 4 boje
+;$3F00		Universal background color
+;$3F01 - $3F03	Bckg palette 0
+;$3F05 - $3F07	Bckg palette 1
+;$3F09 - $3F0B	Bckg palette 2
+;$3F0D - $3F0F	Bckg palette 3
+;$3F11 - $3F13	Sprite palette 0
+;$3F15 - $3F17	Sprite palette 1
+;$3F19 - $3F1B	Sprite palette 2
+;$3F1D - $3F1F	Sprite palette 3
+
+palette_data_bckg:
+	.byte	$2B, $11, $1B, $1C
+	.byte	$2B, $15, $11, $20
+	.byte	$2B, $25, $26, $29
+	.byte	$2B, $30, $38, $3A
+		
+palette_data_sprite:
+	.byte	$2B, $10, $25, $2A
+	.byte	$2B, $31, $32, $3B
+	.byte	$2B, $3C, $3B, $38
+	.byte	$2B, $36, $35, $24
+
+sprite_data:
+	.byte 	$08, $00, $00, $08
+	.byte	$08, $02, $00, $10
+	.byte 	$10, $03, $00, $08
+	.byte	$10, $04, $00, $10
+	.byte 	$18, $05, $00, $08
+	.byte	$18, $06, $00, $10
+	.byte 	$20, $07, $00, $08
+	.byte	$20, $08, $00, $10
 
 .segment "VECTORS"
+	.org $FFFA
+	.word NMI
+	.word RST
+	;defined break interrupt, not using it
 
 .segment "CHARS"
+	.incbin "../chr/main.chr"
